6409ed8dd04550dcd4de7c3286c1506f0cbb5b4e
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-07-26 21:47:36 +0700

    Timeline Map responsive!

--- src/components/TimelineMap/MapBox.jsx
+++ src/components/TimelineMap/MapBox.jsx
@@ -1,37 +1,37 @@
 import { motion } from 'framer-motion';
 
-const MapBox = ({ yPos, xPos, title1, title2, subtitle, num, numPos }) => {
+const MapBox = ({ yPos, xPos, yPosMobile, xPosMobile, title1, title2, subtitle, num, numPos, numPosMobile, customRule }) => {
 	return (
 		<motion.div
-			className={`absolute z-30 h-36 w-80 border-4 border-[#6f2920] bg-[#FFE37F] bg-opacity-50 ${xPos} ${yPos} flex flex-col items-center justify-center text-center`}
+			className={`absolute z-30 md:h-36 md:!w-80 h-16 w-40 md:border-4 border-2 border-[#6f2920] bg-[#FFE37F] bg-opacity-50 ${xPos} ${yPos} ${xPosMobile} ${yPosMobile} ${customRule}  flex flex-col items-center justify-center text-center`}
 			initial={{ opacity: 0, scale: 0.8 }}
 			whileInView={{ opacity: 1, scale: 1 }}
 			transition={{ duration: 0.3, ease: 'easeOut' }}
 			whileHover={{ scale: 1.15 }}
-			viewport={{ margin: '-150px', once: false }}
+			viewport={{ margin: '-30px', once: false }}
 		>
 			<p
-				className='font-sunborn text-3xl font-black leading-7 text-[#f7f3e4]'
+				className='font-sunborn md:text-3xl text-md font-black md:leading-7 leading-4 tracking-tighter md:tracking-normal text-[#f7f3e4]'
 				style={{
 					WebkitTextStrokeColor: '#440A00',
-					WebkitTextStrokeWidth: '0.3px',
+					WebkitTextStrokeWidth: '0.5px',
 				}}
 			>
 				{title1}
 				<br />
 				{title2}
 			</p>
-			<p className='text-lg font-semibold italic text-[#440A00]'>
+			<p className='md:text-lg text-[0.8rem] font-semibold italic text-[#440A00]'>
 				{subtitle}
 			</p>
 			<div
-				className={`absolute -top-1 ${numPos} flex h-10 w-10 items-center justify-center border-4 border-[#6f2920]`}
+				className={`absolute md:-top-1 -top-0.5 ${numPosMobile} ${numPos} flex md:h-10 md:w-10 h-[20px] w-[20px] items-center justify-center md:border-4 border-2 border-[#6f2920]`}
 			>
 				<p
-					className='font-sunborn text-2xl font-black text-[#f7f3e4]'
+					className='font-sunborn md:text-2xl text-xs font-black text-[#f7f3e4]'
 					style={{
 						WebkitTextStrokeColor: '#440A00',
-						WebkitTextStrokeWidth: '0.3px',
+						WebkitTextStrokeWidth: '0.5px',
 					}}
 				>
 					{num}
--- src/components/TimelineMap/TimelineSection.jsx
+++ src/components/TimelineMap/TimelineSection.jsx
@@ -1,7 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
 import map from '../../assets/map.png';
 import MapBox from './MapBox';
 
 const TimelineSection = () => {
+	const [strokeWidth, setStrokeWidth] = useState('0.3px');
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 768) {
+				// For md and larger devices
+				setStrokeWidth('0.3px');
+			} else {
+				// For smaller devices
+				setStrokeWidth('1.3px');
+			}
+		};
+
+		// Set the initial viewport margin
+		handleResize();
+
+		// Add event listener for window resize
+		window.addEventListener('resize', handleResize);
+
+		// Clean up the event listener on component unmount
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
 	return (
 		<div className='relative z-10 flex h-auto w-full bg-[#f7f3e4]'>
 			<img
@@ -10,65 +35,84 @@ const TimelineSection = () => {
 				alt='Map Timeline'
 			/>
 			<p
-				className='absolute left-[10%] top-[4%] font-sunborn text-7xl font-black text-[#FFE37F]'
+				className='absolute left-[5%] top-[-5%] font-sunborn text-5xl font-black text-[#FFE37F] md:left-[10%] md:top-[4%] md:text-7xl'
 				style={{
 					WebkitTextStrokeColor: '#440A00',
-					WebkitTextStrokeWidth: '0.3px',
+					WebkitTextStrokeWidth: strokeWidth,
 				}}
 			>
 				OUR TIMELINE
 			</p>
 			<MapBox
-				xPos='right-[10%]'
-				yPos='top-[10%]'
+				xPos='md:right-[13%]'
+				yPos='md:top-[10%]'
+				xPosMobile='right-[3%]'
+				yPosMobile='top-[3.6%]'
 				title1='MIPA OPEN'
 				title2='HOUSE'
 				subtitle='September - Oktober 2024'
 				num='1'
-				numPos='-right-1'
+				numPos='md:-right-1'
+				numPosMobile='-right-0.5'
 			/>
 			<MapBox
-				xPos='left-[14%]'
-				yPos='top-[24%]'
-				title1='MIPA ON THE'
-				title2='WAY'
+				xPos='md:left-[14%]'
+				yPos='md:top-[24%]'
+				xPosMobile='left-[3%]'
+				yPosMobile='top-[21%]'
+				title1='MIPA ON'
+				title2='THE WAY'
 				subtitle='29 Juli - 23 Agustus 2024'
 				num='2'
-				numPos='-left-1'
+				numPos='md:-left-1'
+				numPosMobile='-left-0.5'
 			/>
 			<MapBox
-				xPos='right-[9%]'
-				yPos='top-[41%]'
+				xPos='md:right-[9%]'
+				yPos='md:top-[41%]'
+				xPosMobile='right-[5%]'
+				yPosMobile='top-[40%]'
 				title1='BEYOND THE'
 				title2='BOOKS'
 				subtitle='9 November 2024'
 				num='3'
-				numPos='-right-1'
+				numPos='md:-right-1'
+				numPosMobile='-right-0.5'
 			/>
 			<MapBox
-				xPos='left-[9%]'
-				yPos='top-[49.8%]'
-				title1='MAPPING YOUR'
-				title2='WAY'
+				xPos='md:left-[9%]'
+				yPos='md:top-[49.8%]'
+				xPosMobile='left-[3%]'
+				yPosMobile='top-[51%]'
+				title1='MAPPING'
+				title2='YOUR WAY'
 				subtitle='9 November 2024'
 				num='4'
-				numPos='-left-1'
+				numPos='md:-left-1'
+				numPosMobile='-left-0.5'
 			/>
 			<MapBox
-				xPos='right-[15%]'
-				yPos='top-[70%]'
+				xPos='md:right-[15%]'
+				yPos='md:top-[70%]'
+				xPosMobile='right-[5%]'
+				yPosMobile='top-[75%]'
 				title1='MIPADVENTOUR!'
 				subtitle='9 November 2024'
 				num='5'
-				numPos='-right-1'
+				numPos='md:-right-1'
+				numPosMobile='-right-0.5'
+				customRule='!w-44'
 			/>
 			<MapBox
-				xPos='left-[42%]'
-				yPos='top-[90%]'
+				xPos='md:left-[42%]'
+				yPos='md:top-[90%]'
+				xPosMobile='left-[14%]'
+				yPosMobile='top-[93%]'
 				title1='INTO THE LAB!'
 				subtitle='9 November 2024'
 				num='6'
-				numPos='-left-1'
+				numPos='md:-left-1'
+				numPosMobile='-left-0.5'
 			/>
 		</div>
 	);
