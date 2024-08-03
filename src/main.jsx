387f5655ec42145c90a71798c1784afa6b841625
Tianrider <hadiwijayachristian7@gmail.com>
2024-08-03 19:12:55 +0700

    feat: Add table scrollbar styling and update npm dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,35 @@
 import './styles.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Admin from './pages/Admin';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import AdminDashboard from './pages/AdminDashboard';
+// import ProtectedRoute from './utils/ProtectedRoutes';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<BrowserRouter>
+		<Routes>
+			<Route
+				path='/'
+				element={<App />}
+			/>
+			<Route
+				path='/admin'
+				element={
+					<Navigate
+						to='/admin/login'
+						replace
+					/>
+				}
+			/>
+			<Route
+				path='/admin/login'
+				element={<Admin />}
+			></Route>
+			<Route
+				path='/admin/dashboard'
+				element={<AdminDashboard />}
+			></Route>
+		</Routes>
+	</BrowserRouter>
+);
