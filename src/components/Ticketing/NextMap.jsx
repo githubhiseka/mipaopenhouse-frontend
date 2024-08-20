e8eee19ee5812dd6066cca0517286b5b5eb616d5
githubhiseka <susantoabhiseka@gmail.com>
2024-08-20 07:57:19 +0700

    removed keterangan 50 pembeli pertama and changed price to 60k

--- src/components/Ticketing/NextMap.jsx
+++ src/components/Ticketing/NextMap.jsx
@@ -8,11 +8,11 @@ export default function NextMap({ nextFunction, showKetentuan }) {
 	return (
 		<div className='absolute bottom-[-40vw] right-[-10vw] h-auto w-full md:bottom-[-25vw] md:w-[60%]'>
 			<div className='relative h-full w-full'>
-				{showKetentuan && (
+				{/* {showKetentuan && (
 					<p className='absolute left-[-7vw] top-[-15vw] -rotate-6 transform text-[#eaffd3] md:fixed md:left-[1vw] md:top-[96%] md:rotate-0 md:text-lg'>
 						*Berlaku untuk 50 pembeli pertama
 					</p>
-				)}
+				)} */}
 				<img
 					src={nextMap}
 					alt=''
