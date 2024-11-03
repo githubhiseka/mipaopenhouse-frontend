2556a034da51985d8546286ddaaf67d04b03bd91
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-11-03 09:26:39 +0700

    Changed contact (2)

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -12,17 +12,12 @@ const ContactSection = () => {
 	return (
 		<div
 			id='contact-section'
-			className='relative flex w-full flex-col items-center justify-center bg-[#283C26] font-lato text-[#FEFDF6]'
-		>
+			className='relative flex w-full flex-col items-center justify-center bg-[#283C26] font-lato text-[#FEFDF6]'>
 			<div className='pt-8'>
-				<p className='text-center text-5xl font-extrabold'>
-					GET IN TOUCH
-				</p>
-				<p className='text-center text-xl font-medium tracking-wider'>
-					Journey to the Universe of Science
-				</p>
+				<p className='text-center text-5xl font-extrabold'>GET IN TOUCH</p>
+				<p className='text-center text-xl font-medium tracking-wider'>Journey to the Universe of Science</p>
 			</div>
-			<div className='flex w-full flex-row flex-wrap gap-y-6 py-8 px-8 sm:flex-nowrap md:px-32'>
+			<div className='flex w-full flex-row flex-wrap gap-y-6 px-8 py-8 sm:flex-nowrap md:px-32'>
 				<div className='order-3 w-full sm:order-1 sm:w-[45%]'>
 					<div className='flex flex-row items-center space-x-2'>
 						<IconMapPin size={22} />
@@ -41,23 +36,17 @@ const ContactSection = () => {
 						<IconPhone size={22} />
 						<p className='text-xl font-bold'>Contact Us</p>
 					</div>
-					<p className='font-[550]'>Venida</p>
+					<p className='font-[550]'>Syabil Syuhada</p>
 					<p>
 						WhatsApp:{' '}
-						<a
-							href='https://wa.me/+62816889740'
-							className='underline underline-offset-2'
-						>
-							0816889740
+						<a href='https://wa.me/+62816889740' className='underline underline-offset-2'>
+							085849340512
 						</a>
 					</p>
 					<p>
 						LINE:{' '}
-						<a
-							href='https://line.me/ti/p/g5HoF1Mmtv'
-							className='underline underline-offset-2'
-						>
-							venidaarmd
+						<a href='https://line.me/ti/p/g5HoF1Mmtv' className='underline underline-offset-2'>
+							rrahman.lutfi
 						</a>
 					</p>
 				</div>
@@ -80,9 +69,7 @@ const ContactSection = () => {
 					<p>#ExploreTheScience</p>
 				</div>
 			</div>
-			<p className='my-3 text-xs'>
-				© FMIPACIL 2024 — All rights reserved.
-			</p>
+			<p className='my-3 text-xs'>© FMIPACIL 2024 — All rights reserved.</p>
 		</div>
 	);
 };
